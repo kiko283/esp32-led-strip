d27e89da207f89e3d2d6e2e56829a84b5f95c94d
Kristijan Kovilevski <kristijan.kovilevski@gmail.com>
2024-08-10 01:56:47 +0200

    refactor secret example file

--- Secret.example.h
+++ Secret.example.h
@@ -1,20 +1,20 @@
 
 // ##### VARIABLES NEEDED FOR OTA #####
 
-const int NETWORKS_COUNT = 1;
+const int NETWORKS_COUNT = 3;
 
 // Network(s) setup
 // You can add more than one if you plan
 // to use device in multiple networks
 const char* SSIDs[NETWORKS_COUNT] = {
-  "SSID1"
-  // "SSID2"
-  // "SSID3"
+  "SSID1",
+  "SSID2",
+  "SSID3"
 };
 const char* passwords[NETWORKS_COUNT] = {
   "password1",
-  // "password2"
-  // "password3"
+  "password2",
+  "password3"
 };
 
 // OTA upgrade page basic auth credentials
